42dc44824cfd119b855b50689801fd93f98acc97
Taylan Pince <taylanpince@gmail.com>
2011-06-15 12:31:05 -0400

    Removed static directory and moved install js to a template

--- tagging_autocomplete/widgets.py
+++ tagging_autocomplete/widgets.py
@@ -1,26 +1,33 @@
-from django.forms.widgets import TextInput
-from django.core.urlresolvers import reverse
 from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.forms.widgets import TextInput
+from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 
 
 class TagAutocomplete(TextInput):
+    def render(self, name, value, attrs=None):
+        if not "class" in attrs:
+            attrs.update({
+                "class" : "vTextField"
+            })
 
-	def render(self, name, value, attrs=None):
-		list_view = reverse('tagging_autocomplete-list')
-		if not 'class' in attrs:
-			attrs.update({'class' : 'vTextField'})
-		html = super(TagAutocomplete, self).render(name, value, attrs)
-		js = u'<script type="text/javascript">$(function() {installTaggingAutocompleate("%s", "%s") });</script>' % (attrs['id'], list_view)
-		return mark_safe("\n".join([html, js]))
+        html = super(TagAutocomplete, self).render(name, value, attrs)
 
-	class Media:
-		js_base_url = getattr(settings, 'TAGGING_AUTOCOMPLETE_JS_BASE_URL', '%sjs/' % settings.STATIC_URL)
-		js = (
-			#'lib/jquery/jquery.min.js',
-			'%sjquery-ui.min.js' % js_base_url,
-			#'%stagging_autocomplete.js' % js_base_url,
-		)
-		css = {
-			'screen': ('css/jquery-ui.css',),
-		}
+        js = render_to_string("tagging_autocomplete/install.html", {
+            "field_id": attrs.get("id"),
+            "api_url": reverse("tagging_autocomplete_list"),
+        })
+
+        return mark_safe("\n".join([html, js]))
+
+    class Media:
+        js_base_url = getattr(settings, 'TAGGING_AUTOCOMPLETE_JS_BASE_URL', '%sjs/' % settings.STATIC_URL)
+
+        js = (
+            '%sjquery-ui.min.js' % js_base_url,
+        )
+
+        css = {
+            'screen': ('css/jquery-ui.css',),
+        }
